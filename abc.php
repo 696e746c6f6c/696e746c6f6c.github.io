e8262ba04708ee7281a10b72eafcb545d09d2666
696e746c6f6c <96709073+696e746c6f6c@users.noreply.github.com>
2025-08-01 23:58:09 +0200

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,4 +1,3 @@
 <?php
-header("Location: https://ssrf-bait.byted.org/blind-ssrf/9998e67b5c43df9f9660bfc28fb74883", TRUE, 301);
-exit();
+header("Location: https://ssrf-bait.byted.org/blind-ssrf/9998e67b5c43df9f9660bfc28fb74883");
 ?>
